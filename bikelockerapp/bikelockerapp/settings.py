fdbd3b4ed44ed862a540a7c5cb6e811948d4e294
Alessandro Lou <alemau@uw.edu>
2020-05-18 10:57:25 -0700

    Loading static attempt

--- bikelockerapp/bikelockerapp/settings.py
+++ bikelockerapp/bikelockerapp/settings.py
@@ -144,10 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
-
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
