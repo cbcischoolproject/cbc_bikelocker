5d62cce69c293512ba4ddbb2de7865008ac6a331
Alessandro Lou <alemau@uw.edu>
2020-05-19 00:21:17 -0700

    Navbar

--- bikelockerapp/database/models.py
+++ bikelockerapp/database/models.py
@@ -105,7 +105,7 @@ class Locker(models.Model):
     locker_name_full = property(my_property)
 
     class Meta:
-        ordering = ['location_id', '-locker_name']
+        ordering = ['-locker_name', 'location_id']
 
 
 class Key_Status(models.Model):
