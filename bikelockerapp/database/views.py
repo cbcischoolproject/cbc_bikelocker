2f9116cd809eaeb15218e616919c49d4c42c3aef
Alessandro Lou <alemau@uw.edu>
2020-05-19 05:17:56 -0700

    Experiment

--- bikelockerapp/database/views.py
+++ bikelockerapp/database/views.py
@@ -19,7 +19,7 @@ def index(request):
     all_station = Location.objects.all()
     all_customer = Customer.objects.all()
     all_cust_locker = Cust_Locker.objects.all()
-    all_renewals = []
+    all_renewals = Cust_Locker.objects.all()
     all_maintenance = Maintenance.objects.all()
 
     # Unqueired Flag
@@ -89,6 +89,7 @@ def index(request):
     if(type(all_cust_locker)) != set and filter_by_location == False:
         if not filter_cust_locker_by_name and filter_inquiry_by_name:
             all_cust_locker = []
+            contains_locker_renewals = False
         else:
             all_cust_locker = all_cust_locker[:5]
 
