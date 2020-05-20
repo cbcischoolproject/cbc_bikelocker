7b0b1e6cb82db758e03e335ea53862c586b383c4
Alessandro Lou <alemau@uw.edu>
2020-05-20 10:28:16 -0700

    Cleaned up views

--- bikelockerapp/database/urls.py
+++ bikelockerapp/database/urls.py
@@ -8,7 +8,6 @@ admin.site.index_title = 'Cascade Bicycle Club Locker Management'
 urlpatterns = [
     path('', views.index, name='index'),
     path('customer_upload/', views.customer_upload, name='customer-upload'),
-    path('customer_waitlist/', views.customer_waitlist, name='customer-waitlist'),
     path('renewals', views.renewals, name='renewals'),
     path('send_email', views.send_email, name='send-email'),
 ]
